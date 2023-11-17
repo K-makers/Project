9cce08235456c4d1b71420028d4e9fd5d5414977
K-makers <119729259+K-makers@users.noreply.github.com>
2023-11-17 16:18:28 +0800

    Update MainActivity3.java

--- app/src/main/java/com/example/project/MainActivity3.java
+++ app/src/main/java/com/example/project/MainActivity3.java
@@ -9,6 +9,6 @@ public class MainActivity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);hghghgh
+        setContentView(R.layout.activity_main3);
     }
 }
